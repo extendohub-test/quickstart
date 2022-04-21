ea3b6eff2c9d5b4baf78b115c2d1c1a9b0d876e2
Jeff McAffer <jeffmcaffer@github.com>
2022-04-21 05:02:29 +0000

    first basic logic

---
+++ .github/apps/hubble/events/issue_comment/created.js
@@ -0,0 +1,10 @@
+import { kv } from '@extendohub/storage'
+
+export default async ({ event }) => {
+  const newWords = event.comment.body.match(/\b\w{5}\b/g)
+  if (!newWords || !newWords.length) return
+  const currentWords = await kv.get('words')
+  const words = new Set(currentWords)
+  newWords.forEach(word => words.add(word))
+  await kv.set('words', Array.from(words))
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# quickstart
-Quickstart demo for using ExtendoHub
+# ExtendoHub quickstart
+
+This repo shows how to create a simple version of Wordle for GitHub users where the words to be guessed are taken from the issue 
+comments in the associated repo. Each repo has a unique set of words and each user plays their own game.
